fee5fb53f4cffb30c0e87271b0325a080d6034aa
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-23 19:04:48 +0400

    Домашнее задание - 3. Задания 1 - 2.

---
+++ src/homework3/Tasks.java
@@ -0,0 +1,23 @@
+package homework3;
+
+public class Tasks {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание - 3. Задание 1.");
+        int CurrentYear = 0;
+        int BeforeCurrentYear = 2022 - 200;
+        int AfterCurrentYear = 2022 + 100;
+        for (int Year = BeforeCurrentYear; Year <= AfterCurrentYear ; Year++) {
+            if (Year % 79 == 0) {
+                System.out.println(Year);
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Домашнее задание - 3. Задание 2.");
+        int Number = 2;
+        int Result = Number;
+        for (int Power = 1; Power <= 10 ; Power++){
+            Result = Number * Power;
+            System.out.println(Number + " * " + Power + " = " + Result);
+        }
+    }
+}
