9dca13fd9686b8859080d2c55a5c3416265f8956
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-26 12:48:38 +0400

    Домашнее задание - 3. Задания 1 - 2. Исправил написание.

--- src/homework1/Tasks.java
+++ src/homework1/Tasks.java
@@ -3,14 +3,14 @@ package homework1;
 public class Tasks {
     public static void main(String[] args) {
         System.out.println("Домашнее задание - 1. Задание 1.");
-        int Salary = 15000;
-        int Total = 0;
+        int salary = 15000;
+        int total = 0;
         int i = 0;
         while (Total <= 2_459_000) {
-            Total = Total + Total/100;
-            Total = Total + Salary;
+            total = total + total/100;
+            total = total + salary;
             i++;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + Total + " рублей.");
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
         }
         System.out.println(" ");
         System.out.println("Домашнее задание - 1. Задание 2.");
@@ -27,15 +27,15 @@ public class Tasks {
         System.out.println(" ");
         System.out.println(" ");
         System.out.println("Домашнее задание - 1. Задание 3.");
-        int CountryY = 12_000_000;
-        int NewCountryY = 0;
-        int BirthRate = (12_000_000 / 1_000) * 17;
-        int Mortality = (12_000_000 / 1_000) * 8;
-        int Difference = BirthRate - Mortality;
-        NewCountryY = NewCountryY + CountryY;
+        int countryY = 12_000_000;
+        int newCountryY = 0;
+        int birthRate = (12_000_000 / 1_000) * 17;
+        int mortality = (12_000_000 / 1_000) * 8;
+        int difference = birthRate - mortality;
+        newCountryY = newCountryY + countryY;
         for (int g = 2022; g <= 2032; g++) {
-            NewCountryY = NewCountryY + Difference;
-            System.out.println("Год " + g + " численность населения составляет " + NewCountryY + ".");
+            newCountryY = newCountryY + difference;
+            System.out.println("Год " + g + " численность населения составляет " + newCountryY + ".");
         }
     }
 }
