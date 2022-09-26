f269bebdbea5e657c8c74c215e30c38b595ebc37
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-26 13:10:03 +0400

    Домашнее задание - 2. Задания 1 - 4. Исправил написание.

--- src/homework2/Tasks.java
+++ src/homework2/Tasks.java
@@ -3,45 +3,45 @@ package homework2;
 public class Tasks {
     public static void main(String[] args) {
         System.out.println("Домашнее задание - 2. Задание 1.");
-        int Deposit = 15_000;
-        int Mont = 0;
-        while (Deposit <= 12_000_000){
-            int InterestRate = Deposit / 100 * 7;
-            Deposit = Deposit+InterestRate;
-            Mont++;
-            System.out.println("Месяц " + Mont + " сумма накоплений равна " + Deposit);
+        int deposit = 15_000;
+        int mont = 0;
+        while (deposit <= 12_000_000){
+            int interestRate = deposit / 100 * 7;
+            deposit = deposit+interestRate;
+            mont++;
+            System.out.println("Месяц " + mont + " сумма накоплений равна " + deposit);
         }
-        System.out.println("Для того что бы Василий накопил 12 миллионов рублей ему понадобиться " + Mont + " месяцев");
+        System.out.println("Для того что бы Василий накопил 12 миллионов рублей ему понадобиться " + mont + " месяцев");
         System.out.println(" ");
         System.out.println("Домашнее задание - 2. Задание 2.");
-        int Deposit2 = 15_000;
-        int Mont2 = 0;
-        while (Deposit2 <= 12_000_000){
-            int InterestRate = Deposit2 / 100 * 7;
-            Deposit2 = Deposit2+InterestRate;
-            Mont2++;
-            if (Mont2 % 6 == 0){
-                System.out.println("Месяц " + Mont2 + " сумма накоплений равна " + Deposit2);
+        int deposit2 = 15_000;
+        int mont2 = 0;
+        while (deposit2 <= 12_000_000){
+            int interestRate = deposit2 / 100 * 7;
+            deposit2 = deposit2+interestRate;
+            mont2++;
+            if (mont2 % 6 == 0){
+                System.out.println("Месяц " + mont2 + " сумма накоплений равна " + deposit2);
             }
         }
         System.out.println(" ");
         System.out.println("Домашнее задание - 2. Задание 3.");
-        int Deposit3 = 15_000;
-        int Mont3 = 0;
-        while (Mont3 <= 107){
-            int InterestRate = Deposit3 / 100 * 7;
-            Deposit3 = Deposit3+InterestRate;
-            Mont3++;
-            if (Mont3 % 6 == 0){
-                System.out.println("Месяц " + Mont3 + " сумма накоплений равна " + Deposit3);
+        int deposit3 = 15_000;
+        int mont3 = 0;
+        while (mont3 <= 107){
+            int interestRate = deposit3 / 100 * 7;
+            deposit3 = deposit3+interestRate;
+            mont3++;
+            if (mont3 % 6 == 0){
+                System.out.println("Месяц " + mont3 + " сумма накоплений равна " + deposit3);
            }
         }
         System.out.println(" ");
         System.out.println("Домашнее задание - 2. Задание 4.");
-        int FridayIndex = 2;
-        while (FridayIndex <= 31){
-                System.out.println("Сегодня пятница, "+ FridayIndex +"-е число. Необходимо подготовить отчет.");
-            FridayIndex = FridayIndex + 7;
+        int fridayIndex = 2;
+        while (fridayIndex <= 31){
+                System.out.println("Сегодня пятница, "+ fridayIndex +"-е число. Необходимо подготовить отчет.");
+            fridayIndex = fridayIndex + 7;
         }
     }
 }
