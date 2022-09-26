3dabe4d7dceedef79dbe1c92a938b413483f16f3
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-26 13:06:07 +0400

    Домашнее задание - 3. Задания 1 - 2. Исправил написание.

--- src/homework3/Tasks.java
+++ src/homework3/Tasks.java
@@ -3,21 +3,21 @@ package homework3;
 public class Tasks {
     public static void main(String[] args) {
         System.out.println("Домашнее задание - 3. Задание 1.");
-        int CurrentYear = 2022;
-        int BeforeCurrentYear = CurrentYear - 200;
-        int AfterCurrentYear = CurrentYear + 100;
-        for (int Year = BeforeCurrentYear; Year <= AfterCurrentYear ; Year++) {
-            if (Year % 79 == 0) {
-                System.out.println(Year);
+        int currentYear = 2022;
+        int beforeCurrentYear = currentYear - 200;
+        int afterCurrentYear = currentYear + 100;
+        for (int year = beforeCurrentYear; year <= afterCurrentYear ; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
             }
         }
         System.out.println(" ");
         System.out.println("Домашнее задание - 3. Задание 2.");
-        int Number = 2;
-        int Result = Number;
-        for (int Power = 1; Power <= 10 ; Power++){
-            Result = Number * Power;
-            System.out.println(Number + " * " + Power + " = " + Result);
+        int number = 2;
+        int result = number;
+        for (int power = 1; power <= 10 ; power++){
+            result = number * power;
+            System.out.println(number + " * " + power + " = " + result);
         }
     }
 }
